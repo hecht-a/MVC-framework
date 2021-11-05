85e7494d45b7d49dfb390b986d083ab8293cba8e
HECHT Axel <axel.hct@gmail.com>
2021-11-05 14:18:49 +0100

    STYLE: profile + footer style separated from others

---
+++ src/views/user/profile.php
@@ -0,0 +1,16 @@
+<?php
+$this->title = "Profil";
+?>
+
+<div class="profile__container">
+    <div>
+        <h1>Profile</h1>
+
+        <div class="inline">
+            <p>Nom: {{firstname}}</p>
+            <p>Prénom: {{lastname}}</p>
+        </div>
+        <p>E-mail: {{email}}</p>
+        <p>Téléphone: {{tel}}</p>
+    </div>
+</div>
