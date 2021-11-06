c6516a9c13f12c7276a1fc08a89f591d00972c9a
HECHT Axel <axel.hct@gmail.com>
2021-11-06 19:02:39 +0100

    CHORE: add INSERT into second migration and add 1 table

--- src/migrations/m0002_sprint2.php
+++ src/migrations/m0002_sprint2.php
@@ -15,17 +15,36 @@ class m0002_sprint2
                   `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
                   `animal` int(11) NOT NULL,
                   `user` int(11) NOT NULL,
+                  `type_consultation` int(11) NOT NULL,
                   `problem` longtext NOT NULL,
                   `date_prise_rdv` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                   `date_rdv` timestamp NOT NULL
                 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+                CREATE TABLE IF NOT EXISTS `type_consultation` (
+                  `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                  `consultation` varchar(255) NOT NULL
+                ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
                 ALTER TABLE `consultations`
                   ADD CONSTRAINT `fk_type_animal` FOREIGN KEY (`animal`) REFERENCES `animaux` (`id`),
-                  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+                  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+                  ADD CONSTRAINT `fk_type_consultation` FOREIGN KEY (`type_consultation`) REFERENCES `type_consultation` (`id`);
                 COMMIT;
+
+                INSERT INTO type_consultation (consultation) VALUES ("hygiene");
+                INSERT INTO type_consultation (consultation) VALUES ("bilan");
+                INSERT INTO type_consultation (consultation) VALUES ("boiterie");
+                INSERT INTO type_consultation (consultation) VALUES ("inconnu");
+                INSERT INTO type_consultation (consultation) VALUES ("urgence");
+
+                INSERT INTO animaux (type_animal) VALUES ("chien");
+                INSERT INTO animaux (type_animal) VALUES ("chat");
+                INSERT INTO animaux (type_animal) VALUES ("oiseau");
+                INSERT INTO animaux (type_animal) VALUES ("rongeur");
+                INSERT INTO animaux (type_animal) VALUES ("reptile");
                 SQL;
-    
+        
         $db->pdo->exec($SQL);
     }
     
