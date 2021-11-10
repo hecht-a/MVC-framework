0429588ae15232e159809bf951c375daed00c03d
HECHT Axel <axel.hct@gmail.com>
2021-11-10 01:36:25 +0100

    FEAT: add `group` method, edit `get` an `post` method to match with the new `group` method

--- src/core/Router.php
+++ src/core/Router.php
@@ -10,6 +10,7 @@ class Router
     public Request $request;
     public Response $response;
     protected array $routes = [];
+    private array $openedGroups = [];
     
     public function __construct(Request $request, Response $response)
     {
@@ -17,14 +18,37 @@ class Router
         $this->response = $response;
     }
     
+    private function dropSlash(string $input): string
+    {
+        if ($input === "/") {
+            return $input;
+        }
+        return preg_replace("/\/\//", "/", "/" . preg_replace("/(\/)$/", "", preg_replace("/^\//", "", $input)));
+    }
+    
+    private function getRecentGroup()
+    {
+        return $this->openedGroups[count($this->openedGroups) - 1] ?? [];
+    }
+    
     public function get(string $path, $callback)
     {
-        $this->routes["get"][$path] = $callback;
+        $openedGroup = $this->getRecentGroup();
+        if ($openedGroup) {
+            $this->routes["get"][$this->dropSlash("$openedGroup/" . $path)] = $callback;
+        } else {
+            $this->routes["get"][$path] = $callback;
+        }
     }
     
     public function post(string $path, $callback)
     {
-        $this->routes["post"][$path] = $callback;
+        $openedGroup = $this->getRecentGroup();
+        if ($openedGroup) {
+            $this->routes["post"][$this->dropSlash("$openedGroup/" . $path)] = $callback;
+        } else {
+            $this->routes["post"][$path] = $callback;
+        }
     }
     
     /**
@@ -32,7 +56,7 @@ class Router
      */
     public function resolve()
     {
-        $path = $this->request->getPath();
+        $path = $this->dropSlash($this->request->getPath());
         $method = $this->request->method();
         $callback = $this->routes[$method][$path] ?? false;
         if (!$callback) {
@@ -48,11 +72,20 @@ class Router
             Application::$app->controller = $controller;
             $controller->action = $callback[1];
             $callback[0] = $controller;
-    
+            
+            
             foreach ($controller->getMiddlewares() as $middleware) {
                 $middleware->execute();
             }
         }
         return call_user_func($callback, $this->request, $this->response);
+    }
+    
+    public function group(string $prefix, callable $callback): self
+    {
+        array_push($this->openedGroups, $prefix);
+        $callback();
+        array_pop($this->openedGroups);
+        return $this;
     }
 }
