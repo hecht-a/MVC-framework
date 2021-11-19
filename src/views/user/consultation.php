9889d7400f71eba2323d4e7ec97582911211e807
HECHT Axel <axel.hct@gmail.com>
2021-11-19 02:43:19 +0100

    CHORE: add button to go to previous page

--- src/views/user/consultation.php
+++ src/views/user/consultation.php
@@ -23,7 +23,14 @@ $rdv = Times::findOne(["id" => $consultation->rdv]);
 ?>
 
 <div class="my__consultations">
-    <h1>Consultation du <?= $rdv ?></h1>
+    <div class="title">
+        <a href="/profile/consultations"><button>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+                </svg>Retour
+            </button></a>
+        <h1>Consultation du <?= $rdv ?></h1>
+    </div>
     <div class="description">
         <div class="item">
             <p>Animal: </p>
