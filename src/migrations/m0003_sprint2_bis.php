1e256aaa899d9739f003531b514fc8a8a912135a
HECHT Axel <axel.hct@gmail.com>
2021-11-20 15:51:20 +0100

    CHORE: add migrations

---
+++ src/migrations/m0003_sprint2_bis.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Core\Application;
+
+class m0003_sprint2_bis
+{
+    public function up()
+    {
+        $db = Application::$app->db;
+        $SQL = <<<SQL
+                DELETE FROM type_consultation;
+
+                INSERT INTO type_consultation (consultation) VALUES ("Vaccination");
+                INSERT INTO type_consultation (consultation) VALUES ("Bilan de santé");
+                INSERT INTO type_consultation (consultation) VALUES ("Problèmes de boiterie");
+                INSERT INTO type_consultation (consultation) VALUES ("Problèmes inconnu");
+                INSERT INTO type_consultation (consultation) VALUES ("Problème digestif");
+                INSERT INTO type_consultation (consultation) VALUES ("Traumatismes divers");
+                INSERT INTO type_consultation (consultation) VALUES ("Dermatologie");
+                INSERT INTO type_consultation (consultation) VALUES ("Sterilisation / Castration");
+                INSERT INTO type_consultation (consultation) VALUES ("Fin de vie");
+                INSERT INTO type_consultation (consultation) VALUES ("Consultation urgente");
+                INSERT INTO type_consultation (consultation) VALUES ("Autre");
+                SQL;
+        
+        $db->pdo->exec($SQL);
+    }
+    
+    public function down()
+    {
+        echo "Down migration" . PHP_EOL;
+    }
+}
