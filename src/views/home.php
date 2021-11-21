9bb27f5f883786696c7e9a65c19feb528b4b12d0
HECHT Axel <axel.hct@gmail.com>
2021-11-21 14:04:42 +0100

    FIX: homepage style

--- src/views/home.php
+++ src/views/home.php
@@ -3,12 +3,10 @@ $this->title = "Accueil";
 ?>
 {{ @layout("main") }}
 <div class="home">
-    <div class="container">
-        <div class="titles">
-            <h1>Docteur vétérinaire Octavien Doiron</h1>
+    <div class="titles">
+        <h1>Docteur vétérinaire Octavien Doiron</h1>
 
-            <h4>24 Chem. des Rouliers , 51100 Reims</h4>
-        </div>
+        <h4>24 Chem. des Rouliers , 51100 Reims</h4>
     </div>
 </div>
 
