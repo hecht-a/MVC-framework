b89dff3ef084309c0af877bd6129bf1d5c9db105
HECHT Axel <axel.hct@gmail.com>
2021-11-23 12:07:47 +0100

    FEAT: add link in profile navbar

--- src/Views/components/profile_navbar.php
+++ src/Views/components/profile_navbar.php
@@ -2,5 +2,6 @@
     <ul>
         <li><a href="/profile">Accueil</a></li>
         <li><a href="/profile/consultations">consultations</a></li>
+        <li><a href="/profile/animals">mes animaux</a></li>
     </ul>
 </div>
